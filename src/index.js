11be6e408625c0b2fdb9d65373f576f3a9ccfcd3
Your NameIlya <kirnichnyilya@gmail.com>
2018-09-12 12:23:14 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(p = []) {
   // your implementation
-};
+  var r = new Set()
+  p.forEach((item, i, arr) =>{
+    var FS = arr[item-1];
+    var SS = arr[FS-1];
+    var TS = arr[SS-1];
+    if (TS==item){
+      [item,FS,SS].map(item =>r.add(item));
+    }
+  });
+    return Math.floor (r.size / 3);
+  };
